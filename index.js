df279fccb367e64325b3d905bfd8f31fac720999
Reece Bernard <reecebernard@MacBook-Pro-3.local>
2024-03-10 13:28:48 -0400

    done

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ function skipIntro() {
     skip.click();
   });
 }
-
 if (location.href && location.href.includes("netflix.com/watch")) {
-  console.log("saw it");
   var mutationCallback = function (mutationsList) {
     for (var mutation of mutationsList) {
       if (mutation.type === "childList") {
-        console.log("running");
         skipIntro();
       }
     }
